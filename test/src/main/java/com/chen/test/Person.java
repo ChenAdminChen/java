246440d2ade73227cb57907ef64aec5e6fe859ea
ChenAdminChen <1742838987@qq.com>
2018-04-26 11:18:43 +0800

    添加test

---
+++ test/src/main/java/com/chen/test/Person.java
@@ -0,0 +1,73 @@
+package com.chen.test;
+
+/**
+ * Created by chen on 2018/4/18.
+ */
+public class Person {
+
+    String name;
+    int age;
+    String describe;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return describe != null ? describe.equals(person.describe) : person.describe == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        result = 31 * result + (describe != null ? describe.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", describe='" + describe + '\'' +
+                '}';
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String describe) {
+        this.name = name;
+        this.age = age;
+        this.describe = describe;
+    }
+}
