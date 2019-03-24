ebf10288ff798a4bac7f12d9d728a0ae7bbbe9c2
ChenAdminChen <1742838987@qq.com>
2019-03-24 21:55:05 +0800

    rxjava2

--- rxjava2/src/main/java/com/chen/demo/Mains.java
+++ rxjava2/src/main/java/com/chen/demo/Mains.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mains {
+
+    //需要toBlocking
+    // 按type分组 求type出现的次数（count） 求type相同时number的和（result）
     public static void main(String[] args) {
         Demo demo = new Demo(1, 2);
         Demo demo1 = new Demo(1, 2);
@@ -16,7 +19,6 @@ public class Mains {
         Demo demo5 = new Demo(2, 2);
         Demo demo6 = new Demo(1, 4);
         Demo demo7 = new Demo(3, 3);
-
 
 
         List<Demo> list = new ArrayList<>();
@@ -29,26 +31,32 @@ public class Mains {
         list.add(demo6);
         list.add(demo7);
 
+        List<Demo> demos = new ArrayList<>();
+
         Observable.fromIterable(list)
                 .sorted()
                 .groupBy(x -> x.getType())
                 .flatMap(r -> r.toList().toObservable())
-                .subscribe(r ->
+                .map(r ->
                         {
                             Demo d = r.get(0);
 
                             d.setCount(r.size());
-                            if(r.size() >= 2){
-                                for (int i = 0; i < r.size(); i++) {
-                                    d.setResult(d.getResult() + r.get(i).getNumber());
-                                }
+
+//                            if(r.size() >= 2){
+                            for (int i = 0; i < r.size(); i++) {
+                                d.setResult(d.getResult() + r.get(i).getNumber());
                             }
+//                            }
+                            return d;
 
-                            System.out.println(d);
                         }
-                );
+                ).blockingIterable()
+                .forEach(r -> list.set(list.size(), r));
 
-
+        for (int i =0; i< list.size();i++){
+            System.out.println(list.get(i));
+        }
     }
 }
 
