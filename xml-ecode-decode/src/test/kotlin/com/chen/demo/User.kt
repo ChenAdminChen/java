a313ae07005432b2c327649782cbd2dc133b9a31
ChenAdminChen <1742838987@qq.com>
2019-09-09 16:39:07 +0800

    xml-ecode-decode

---
+++ xml-ecode-decode/src/test/kotlin/com/chen/demo/User.kt
@@ -0,0 +1,10 @@
+package com.chen.demo
+
+data class User(val name: String? = null, val age: String? = null) {
+
+
+}
+
+data class Address(val address: String? = null, val phone: String? = null) {
+    
+}
---
+++ xml-ecode-decode/src/test/kotlin/com/chen/demo/XmlTest.kt
@@ -0,0 +1,4 @@
+package com.chen.demo
+
+class XmlTest {
+}
